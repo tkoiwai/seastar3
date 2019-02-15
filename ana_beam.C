2ad23b5889c5d5619f8bf4e9f834c7e73f6ca41c
tkoiwai <koiwai@nex.phys.s.u-tokyo.ac.jp>
2019-02-15 18:09:15 +0900

    commented out Nhit and Slew

--- ana_beam.C
+++ ana_beam.C
@@ -122,24 +122,24 @@ int main(int argc, char *argv[]){
   caltr->SetBranchAddress("plaF7_TL",&plaF7_TL);
   caltr->SetBranchAddress("plaF7_TR",&plaF7_TR);
 
-  caltr->SetBranchAddress("plaF3_NhitL",&plaF3_NhitL);
-  caltr->SetBranchAddress("plaF5_NhitL",&plaF5_NhitL);
-  caltr->SetBranchAddress("plaF7_NhitL",&plaF7_NhitL);
-  caltr->SetBranchAddress("plaF3_NhitR",&plaF3_NhitR);
-  caltr->SetBranchAddress("plaF5_NhitR",&plaF5_NhitR);
-  caltr->SetBranchAddress("plaF7_NhitR",&plaF7_NhitR);
-  caltr->SetBranchAddress("plaF3_TslewL",&plaF3_TslewL);
-  caltr->SetBranchAddress("plaF5_TslewL",&plaF5_TslewL);
-  caltr->SetBranchAddress("plaF7_TslewL",&plaF7_TslewL);
-  caltr->SetBranchAddress("plaF3_TslewR",&plaF3_TslewR);
-  caltr->SetBranchAddress("plaF5_TslewR",&plaF5_TslewR);
-  caltr->SetBranchAddress("plaF7_TslewR",&plaF7_TslewR);
-  caltr->SetBranchAddress("plaF3_Tslew",&plaF3_Tslew);
-  caltr->SetBranchAddress("plaF5_Tslew",&plaF5_Tslew);
-  caltr->SetBranchAddress("plaF7_Tslew",&plaF7_Tslew);
-  caltr->SetBranchAddress("plaF3_dTslew",&plaF3_dTslew);
-  caltr->SetBranchAddress("plaF5_dTslew",&plaF5_dTslew);
-  caltr->SetBranchAddress("plaF7_dTslew",&plaF7_dTslew);
+  //caltr->SetBranchAddress("plaF3_NhitL",&plaF3_NhitL);
+  //caltr->SetBranchAddress("plaF5_NhitL",&plaF5_NhitL);
+  //caltr->SetBranchAddress("plaF7_NhitL",&plaF7_NhitL);
+  //caltr->SetBranchAddress("plaF3_NhitR",&plaF3_NhitR);
+  //caltr->SetBranchAddress("plaF5_NhitR",&plaF5_NhitR);
+  //caltr->SetBranchAddress("plaF7_NhitR",&plaF7_NhitR);
+  //caltr->SetBranchAddress("plaF3_TslewL",&plaF3_TslewL);
+  //caltr->SetBranchAddress("plaF5_TslewL",&plaF5_TslewL);
+  //caltr->SetBranchAddress("plaF7_TslewL",&plaF7_TslewL);
+  //caltr->SetBranchAddress("plaF3_TslewR",&plaF3_TslewR);
+  //caltr->SetBranchAddress("plaF5_TslewR",&plaF5_TslewR);
+  //caltr->SetBranchAddress("plaF7_TslewR",&plaF7_TslewR);
+  //caltr->SetBranchAddress("plaF3_Tslew",&plaF3_Tslew);
+  //caltr->SetBranchAddress("plaF5_Tslew",&plaF5_Tslew);
+  //caltr->SetBranchAddress("plaF7_Tslew",&plaF7_Tslew);
+  //caltr->SetBranchAddress("plaF3_dTslew",&plaF3_dTslew);
+  //caltr->SetBranchAddress("plaF5_dTslew",&plaF5_dTslew);
+  //caltr->SetBranchAddress("plaF7_dTslew",&plaF7_dTslew);
   
   caltr->SetBranchAddress("ppacF31A_X",&ppacF31A_X);
   caltr->SetBranchAddress("ppacF31A_Y",&ppacF31A_Y);
@@ -242,18 +242,18 @@ int main(int argc, char *argv[]){
   caltr->SetBranchAddress("sbt2_TL",&sbt2_TL);
   caltr->SetBranchAddress("sbt2_TR",&sbt2_TR);
 
-  caltr->SetBranchAddress("sbt1_NhitL",&sbt1_NhitL);
-  caltr->SetBranchAddress("sbt2_NhitL",&sbt2_NhitL);
-  caltr->SetBranchAddress("sbt1_NhitR",&sbt1_NhitR);
-  caltr->SetBranchAddress("sbt2_NhitR",&sbt2_NhitR);
-  caltr->SetBranchAddress("sbt1_TslewL",&sbt1_TslewL);
-  caltr->SetBranchAddress("sbt2_TslewL",&sbt2_TslewL);    
-  caltr->SetBranchAddress("sbt1_TslewR",&sbt1_TslewR);
-  caltr->SetBranchAddress("sbt2_TslewR",&sbt2_TslewR);    
-  caltr->SetBranchAddress("sbt1_Tslew",&sbt1_Tslew);
-  caltr->SetBranchAddress("sbt2_Tslew",&sbt2_Tslew);    
-  caltr->SetBranchAddress("sbt1_dTslew",&sbt1_dTslew);
-  caltr->SetBranchAddress("sbt2_dTslew",&sbt2_dTslew);
+  //caltr->SetBranchAddress("sbt1_NhitL",&sbt1_NhitL);
+  //caltr->SetBranchAddress("sbt2_NhitL",&sbt2_NhitL);
+  //caltr->SetBranchAddress("sbt1_NhitR",&sbt1_NhitR);
+  //caltr->SetBranchAddress("sbt2_NhitR",&sbt2_NhitR);
+  //caltr->SetBranchAddress("sbt1_TslewL",&sbt1_TslewL);
+  //caltr->SetBranchAddress("sbt2_TslewL",&sbt2_TslewL);    
+  //caltr->SetBranchAddress("sbt1_TslewR",&sbt1_TslewR);
+  //caltr->SetBranchAddress("sbt2_TslewR",&sbt2_TslewR);    
+  //caltr->SetBranchAddress("sbt1_Tslew",&sbt1_Tslew);
+  //caltr->SetBranchAddress("sbt2_Tslew",&sbt2_Tslew);    
+  //caltr->SetBranchAddress("sbt1_dTslew",&sbt1_dTslew);
+  //caltr->SetBranchAddress("sbt2_dTslew",&sbt2_dTslew);
   
   //===== Load CUT files ==================================================
   //=== Plastic (graphical cut)===
