55c47fab97d31934de659894426b53dff63ed154
tkoiwai <koiwai@nex.phys.s.u-tokyo.ac.jp>
2019-05-09 14:18:15 +0900

    typo corrected.

--- hodo_tofcor.C
+++ hodo_tofcor.C
@@ -1,6 +1,7 @@
 //#include "loadsmri.C"
-
-void hodo_tofcor(Int_t runnum, Int_t hodo_id){
+#include <stdio.h>
+#include <typeinfo.h>
+void hodo_tofcor(Int_t runnum, int hodo_id){
 
   gROOT->ProcessLine( "gErrorIgnoreLevel = 2001;");
   
@@ -8,10 +9,14 @@ void hodo_tofcor(Int_t runnum, Int_t hodo_id){
   TString FileNameS = Form("/home/koiwai/analysis/rootfiles/ana/smri/ana_smri%04d.root",runnum);
   TFile *f = TFile::Open(FileNameS);
 
+    
   switch(hodo_id){
+    //cout << "in switch" << endl;
   case 1:
+    cout << "case 1" << endl;
     break;
   case 2:
+    cout << "case 2" << endl;
     //anatrS->Draw("t_minoshodo_notcor>>h(200,55,65)","BG_flag_beam&&hodo_id==2");
     //TF1 *func = new TF1("func","gaus");
     //h->Fit("func");
@@ -21,6 +26,7 @@ void hodo_tofcor(Int_t runnum, Int_t hodo_id){
     //fout << runnum << ":" << tofcor02 << endl;
     break;
   case 3:
+    cout << "case 3" << endl;
     anatrS->Draw("t_minoshodo_notcor>>h(200,55,65)","BG_flag_beam&&hodo_id==3");
     TF1 *func = new TF1("func","gaus");
     h->Fit("func");
@@ -30,6 +36,7 @@ void hodo_tofcor(Int_t runnum, Int_t hodo_id){
     fout << runnum << ":" << tofcor03 << endl;
     break;
   case 4:
+    cout << "case 4" << endl;
     anatrS->Draw("t_minoshodo_notcor>>h(200,55,65)","BG_flag_beam&&hodo_id==04");
     TF1 *func = new TF1("func","gaus");
     h->Fit("func");
@@ -39,6 +46,7 @@ void hodo_tofcor(Int_t runnum, Int_t hodo_id){
     fout << runnum << ":" << tofcor04 << endl;
     break;
   case 5:
+    cout << "case 5" << endl;
     anatrS->Draw("t_minoshodo_notcor>>h(200,55,65)","BG_flag_beam&&hodo_id==5");
     TF1 *func = new TF1("func","gaus");
     h->Fit("func");
@@ -48,6 +56,7 @@ void hodo_tofcor(Int_t runnum, Int_t hodo_id){
     fout << runnum << ":" << tofcor05 << endl;
     break;
   case 6:
+    cout << "case 6" << endl;
     anatrS->Draw("t_minoshodo_notcor>>h(200,55,65)","BG_flag_beam&&hodo_id==6");
     TF1 *func = new TF1("func","gaus");
     h->Fit("func");
@@ -57,6 +66,7 @@ void hodo_tofcor(Int_t runnum, Int_t hodo_id){
     fout << runnum << ":" << tofcor06 << endl;
     break;
   case 7:
+    cout << "case 7" << endl;
     anatrS->Draw("t_minoshodo_notcor>>h(200,55,65)","BG_flag_beam&&hodo_id==7&&br50ar");
     TF1 *func = new TF1("func","gaus");
     h->Fit("func");
@@ -66,6 +76,7 @@ void hodo_tofcor(Int_t runnum, Int_t hodo_id){
     fout << runnum << ":" << tofcor07 << endl;
     break;
   case 8:
+    cout << "case 8" << endl;
     anatrS->Draw("t_minoshodo_notcor>>h(200,57,62)","BG_flag_beam&&hodo_id==8&&br50ar");
     TF1 *func = new TF1("func","gaus");
     h->Fit("func");
@@ -75,6 +86,7 @@ void hodo_tofcor(Int_t runnum, Int_t hodo_id){
     fout << runnum << ":" << tofcor08 << endl;
     break;
   case 9:
+    cout << "case 9" << endl;
     anatrS->Draw("t_minoshodo_notcor>>h(200,55,65)","BG_flag_beam&&hodo_id==9&&br51k");
     TF1 *func = new TF1("func","gaus");
     h->Fit("func");
@@ -84,6 +96,7 @@ void hodo_tofcor(Int_t runnum, Int_t hodo_id){
     fout << runnum << ":" << tofcor09 << endl;
     break;
   case 10:
+    cout << "case 10" << endl;
     anatrS->Draw("t_minoshodo_notcor>>h(200,55,65)","BG_flag_beam&&hodo_id==10&&br51k");
     TF1 *func = new TF1("func","gaus");
     h->Fit("func");
@@ -93,6 +106,7 @@ void hodo_tofcor(Int_t runnum, Int_t hodo_id){
     fout << runnum << ":" << tofcor10 << endl;
     break;
   case 11:
+    cout << "case 11" << endl;
     anatrS->Draw("t_minoshodo_notcor>>h(200,55,65)","BG_flag_beam&&hodo_id==11&&br51k");
     TF1 *func = new TF1("func","gaus");
     h->Fit("func");
@@ -102,6 +116,7 @@ void hodo_tofcor(Int_t runnum, Int_t hodo_id){
     fout << runnum << ":" << tofcor11 << endl;
     break;
   case 12:
+    cout << "case 12" << endl;
     anatrS->Draw("t_minoshodo_notcor>>h(200,55,65)","BG_flag_beam&&hodo_id==12&&br51k");
     TF1 *func = new TF1("func","gaus");
     h->Fit("func");
@@ -109,9 +124,11 @@ void hodo_tofcor(Int_t runnum, Int_t hodo_id){
     if(abs(tofcor12)>10.) break;
     ofstream fout("/home/koiwai/analysis/db/hodo12_tofcor.dat",ios::app);
     fout << runnum << ":" << tofcor12 << endl;
-    brea2
+    break;
   case 13:
+    cout << "case 13" << endl;
     anatrS->Draw("t_minoshodo_notcor>>h(200,55,65)","BG_flag_beam&&hodo_id==13&&br56sc");
+    if(h) cout << "histogram created" << endl;
     TF1 *func = new TF1("func","gaus");
     h->Fit("func");
     Double_t tofcor13 = 57.3169 - func->GetParameter(1);
@@ -120,6 +137,7 @@ void hodo_tofcor(Int_t runnum, Int_t hodo_id){
     fout << runnum << ":" << tofcor13 << endl;
     break;
   case 14:
+    cout << "case 14" << endl;
     anatrS->Draw("t_minoshodo_notcor>>h(200,55,67)","BG_flag_beam&&hodo_id==14&&br58ti");
     TF1 *func = new TF1("func","gaus");
     h->Fit("func");
@@ -129,6 +147,7 @@ void hodo_tofcor(Int_t runnum, Int_t hodo_id){
     fout << runnum << ":" << tofcor14 << endl;
     break;
   case 15:
+    cout << "case 15" << endl;
     anatrS->Draw("t_minoshodo_notcor>>h(200,55,67)","BG_flag_beam&&hodo_id==15&&br61v");
     TF1 *func = new TF1("func","gaus");
     h->Fit("func");
@@ -138,6 +157,7 @@ void hodo_tofcor(Int_t runnum, Int_t hodo_id){
     fout << runnum << ":" << tofcor15 << endl;
     break;
   case 16:
+    cout << "case 16" << endl;
     anatrS->Draw("t_minoshodo_notcor>>h(200,55,67)","BG_flag_beam&&hodo_id==16&&br62v");
     TF1 *func = new TF1("func","gaus");
     h->Fit("func");
@@ -147,6 +167,7 @@ void hodo_tofcor(Int_t runnum, Int_t hodo_id){
     fout << runnum << ":" << tofcor16 << endl;
     break;
   case 17:
+    cout << "case 17" << endl;
     anatrS->Draw("t_minoshodo_notcor>>h(200,55,67)","BG_flag_beam&&hodo_id==17&&br56sc");
     TF1 *func = new TF1("func","gaus");
     h->Fit("func");
@@ -156,6 +177,7 @@ void hodo_tofcor(Int_t runnum, Int_t hodo_id){
     fout << runnum << ":" << tofcor17 << endl;
     break;
   case 18:
+    cout << "case 18" << endl;
     anatrS->Draw("t_minoshodo_notcor>>h(200,55,67)","BG_flag_beam&&hodo_id==18&&br56sc");
     TF1 *func = new TF1("func","gaus");
     h->Fit("func");
@@ -165,6 +187,7 @@ void hodo_tofcor(Int_t runnum, Int_t hodo_id){
     fout << runnum << ":" << tofcor18 << endl;
     break;
   case 19:
+    cout << "case 19" << endl;
     anatrS->Draw("t_minoshodo_notcor>>h(200,55,67)","BG_flag_beam&&hodo_id==19&&br56sc");
     TF1 *func = new TF1("func","gaus");
     h->Fit("func");
@@ -174,6 +197,7 @@ void hodo_tofcor(Int_t runnum, Int_t hodo_id){
     fout << runnum << ":" << tofcor19 << endl;
     break;
   case 20:
+    cout << "case 20" << endl;
     anatrS->Draw("t_minoshodo_notcor>>h(200,55,67)","BG_flag_beam&&hodo_id==20&&br56sc");
     TF1 *func = new TF1("func","gaus");
     h->Fit("func");
@@ -183,6 +207,7 @@ void hodo_tofcor(Int_t runnum, Int_t hodo_id){
     fout << runnum << ":" << tofcor20 << endl;
     break;
   case 21:
+    cout << "case 21" << endl;
     anatrS->Draw("t_minoshodo_notcor>>h(200,55,67)","BG_flag_beam&&hodo_id==21&&br56sc");
     TF1 *func = new TF1("func","gaus");
     h->Fit("func");
@@ -192,6 +217,7 @@ void hodo_tofcor(Int_t runnum, Int_t hodo_id){
     fout << runnum << ":" << tofcor21 << endl;
     break;
   case 22:
+    cout << "case 22" << endl;
     anatrS->Draw("t_minoshodo_notcor>>h(200,55,67)","BG_flag_beam&&hodo_id==22&&br56sc");
     TF1 *func = new TF1("func","gaus");
     h->Fit("func");
@@ -201,6 +227,7 @@ void hodo_tofcor(Int_t runnum, Int_t hodo_id){
     fout << runnum << ":" << tofcor22 << endl;
     break;
   case 23:
+    cout << "case 23" << endl;
     anatrS->Draw("t_minoshodo_notcor>>h(200,55,67)","BG_flag_beam&&hodo_id==23&&br56sc");
     TF1 *func = new TF1("func","gaus");
     h->Fit("func");
@@ -210,6 +237,7 @@ void hodo_tofcor(Int_t runnum, Int_t hodo_id){
     fout << runnum << ":" << tofcor23 << endl;
     break;
   case 24:
+    cout << "case 24" << endl;
     //anatrS->Draw("t_minoshodo_notcor>>h(100,57,77)","BG_flag_beam&&hodo_id==24");
     //TF1 *func = new TF1("func","gaus");
     //h->Fit("func");
@@ -218,8 +246,11 @@ void hodo_tofcor(Int_t runnum, Int_t hodo_id){
     //ofstream fout("/home/koiwai/analysis/db/hodo24_tofcor.dat",ios::app);
     //fout << runnum << ":" << tofcor24 << endl;
     break;
+  default:
+    //cout << id << endl;
+    cout << "default called" << endl;
   }
-
+    
   f->Close();
   
     
